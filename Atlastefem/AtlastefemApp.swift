547a026c0b74f184ade645e1dd7db479ea2b3574
Denver <jmorgaz@gmail.com>
2020-12-12 09:43:02 +0100

    Add Cocoa Pod dependences.

---
+++ Atlastefem/AtlastefemApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlastefemApp.swift
+//  Atlastefem
+//
+//  Created by Jmorgaz on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AtlastefemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Atlastefem-AtlastefemUITests/Pods-Atlastefem-AtlastefemUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Atlastefem_AtlastefemUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Atlastefem_AtlastefemUITests
+@end
